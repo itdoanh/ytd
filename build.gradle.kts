8380653afcac4bd44f23ba55299f4dab07631b0e
Đoàn Văn Doanh <157219955+itdoanh@users.noreply.github.com>
2026-02-25 09:19:57 +0000

    chore: downgrade gradle and java target for java 8

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 // File cấu hình chung cho cả dự án
 plugins {
-    // Khai báo phiên bản 8.1.0 cho Plugin Android (Tương thích Gradle 8.0)
-    id("com.android.application") version "8.1.0" apply false
+    // Dùng AGP 7.4.2 để build với Java 8
+    id("com.android.application") version "7.4.2" apply false
 }
