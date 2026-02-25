5d5e52868fe7e72cfeaee6522b3dea758e4ea4a4
Đoàn Văn Doanh <157219955+itdoanh@users.noreply.github.com>
2026-02-25 10:18:53 +0000

    fix: AGP 8.1.1 match Gradle 8.1.1 version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 // File cấu hình chung cho cả dự án
 plugins {
-    // Dùng AGP 8.2.0 để build với Java 11
-    id("com.android.application") version "8.2.0" apply false
+    // Dùng AGP 8.1.1 để build với Java 11
+    id("com.android.application") version "8.1.1" apply false
 }
