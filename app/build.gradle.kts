26419cf961807e38d46164c9de9924d09564488f
Đoàn Văn Doanh <157219955+itdoanh@users.noreply.github.com>
2026-02-25 11:08:42 +0000

    fix: downgrade dependencies for AGP 4.2.2 compatibility (Material 1.4.0, Glide 4.12.0, etc)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,18 +4,18 @@ plugins {
 
 android {
     namespace = "com.my.downloader"
-    compileSdk = 33
+    compileSdk = 30
 
     defaultConfig {
         applicationId = "com.my.downloader"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 30
         versionCode = 1
         versionName = "1.0"
     }
 
     buildTypes {
-        release {
+        getByName("release") {
             isMinifyEnabled = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
@@ -33,17 +33,17 @@ android {
 }
 
 dependencies {
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.appcompat:appcompat:1.3.1")
+    implementation("com.google.android.material:material:1.4.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.0")
     
     // Thư viện tải ảnh
-    implementation("com.github.bumptech.glide:glide:4.15.1")
+    implementation("com.github.bumptech.glide:glide:4.12.0")
     
     // Thư viện mạng & JSON
-    implementation("com.squareup.okhttp3:okhttp:4.10.0")
-    implementation("com.google.code.gson:gson:2.10.1")
-    implementation("org.json:json:20231013") // Thêm JSON parsing
+    implementation("com.squareup.okhttp3:okhttp:4.9.1")
+    implementation("com.google.code.gson:gson:2.8.8")
+    implementation("org.json:json:20210307") // JSON parsing
 }
 
 repositories {
